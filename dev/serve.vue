e65341215704c5a4e8a11d734a466652f11c364c
Michael Perks <michael.perks@live.co.uk>
2020-08-25 17:26:45 +0100

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueHackerText from '@/vue-hacker-text.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueHackerText
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-hacker-text />
+  </div>
+</template>
---
+++ src/vue-hacker-text.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'VueHackerText',
+}
+</script>
+
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<style scoped>
+
+</style>
